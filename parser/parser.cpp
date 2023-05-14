9680c255b43e7efe77f977a7fab0512b54b0df96
anna806 <tick.anna@gmail.com>
2023-05-14 13:37:03 +0200

    Loading file

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -2,11 +2,59 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void loadFile(string path, char*& data) {
+    streampos size;
+    //char* data;
+    ifstream caff(path, ios::in | ios::binary | ios::ate);
+    if (caff.is_open()) {
+        size = caff.tellg();
+        data = new char[size];
+        caff.seekg(0, ios::beg);
+        caff.read(data, size);
+        caff.close();
+    }
+    else {
+        cout << "Unable to open file\n";
+    }
 }
+
+void processData(char* data) {
+    uint64_t length;
+    switch (data[0]) {
+        case 0x1:
+            length = static_cast<uint64_t>(static_cast<unsigned char>(data[1]) |
+                    static_cast<unsigned char>(data[2]) << 8 |
+                    static_cast<unsigned char>(data[3]) << 16 |
+                    static_cast<unsigned char>(data[4]) <<  24 |
+                    static_cast<unsigned char>(data[5]) <<  32 |
+                    static_cast<unsigned char>(data[6]) <<  40 |
+                    static_cast<unsigned char>(data[7]) <<  48 |
+                    static_cast<unsigned char>(data[8]) <<  56);
+            cout << length;
+            break;
+        default: 
+            cout << "Not found header!";
+            exit(1);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    string path = argv[0];
+    path = "C:\\Users\\ticka\\Documents\\BME\\Szoftverbiztonsag\\caff_files\\1.caff";
+    char* data;
+    loadFile(path, data);
+    if (data != NULL) {
+        processData(data);
+    }
+    return 0;
+}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
